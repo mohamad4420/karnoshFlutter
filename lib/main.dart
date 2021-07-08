dd62a7806302c794c87fd4b5dc8ec5763ded67b9
mohamad abudaya <63648938+mohamad4420@users.noreply.github.com>
2021-07-08 16:48:09 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,30 @@ class MyApp extends StatelessWidget {
         title: 'karnosh',
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("karnsoh"),
-              backgroundColor: Colors.red[400],
-              centerTitle: true,
-            ),
-            endDrawer: Drawer(
-                child: ListView(
-              children: <Widget>[
-                UserAccountsDrawerHeader(
-                  accountName: Text("mohamad abudaya"),
-                  accountEmail: Text("m1152580@gmail.com"),
-                  currentAccountPicture: CircleAvatar(
-                    child: Image(
-                      image: NetworkImage(
-                          'https://lh3.googleusercontent.com/ogw/ADea4I4APSzm8AVe1Kb_t3-9pt8fJXV9EPV3p347d-jp0FI=s500-c-mo'),
-                      fit: BoxFit.cover,
-                    ),
+          appBar: AppBar(
+            title: Text("karnsoh"),
+            backgroundColor: Colors.red[400],
+            centerTitle: true,
+          ),
+          endDrawer: Drawer(
+              child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("mohamad abudaya"),
+                accountEmail: Text("m1152580@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  child: Image(
+                    image: NetworkImage(
+                        'https://lh3.googleusercontent.com/ogw/ADea4I4APSzm8AVe1Kb_t3-9pt8fJXV9EPV3p347d-jp0FI=s500-c-mo'),
+                    fit: BoxFit.cover,
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.red[400],
-                  ),
-                )
-              ],
-            )),
-            drawer: Drawer(text: Text("abodia "))));
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.red[400],
+                ),
+              )
+            ],
+          )),
+        ));
   }
 }
