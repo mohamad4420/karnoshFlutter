35f80b18b32992e0335fb95798f4fa144186eb7d
basheershalabi123 <basheersalaby@gmail.com>
2021-07-09 00:52:03 +0300

    fuk

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mediaQueryData = MediaQuery.of(context);
     return MaterialApp(
         title: 'karnosh',
         debugShowCheckedModeBanner: false,
@@ -31,4 +32,21 @@ class HomePage extends StatelessWidget {
         drawer: DrowerHome(),
         body: Mybody());
   }
+}
+
+String getDeviceType(MediaQueryData mediaQueryData) {
+  Orientation orientation = mediaQueryData.orientation;
+  double width = 0;
+  if (orientation == Orientation.landscape) {
+    width = mediaQueryData.size.height;
+  } else {
+    width = mediaQueryData.size.width;
+  }
+  if (width >= 950) {
+    return "Desktop";
+  }
+  if (width >= 600) {
+    return "Tablet";
+  }
+  return "Mobile";
 }
