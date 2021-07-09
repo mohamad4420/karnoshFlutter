c2b0430ad60ab4d79b08e05abbd65cdc5037b5f2
basheershalabi123 <basheersalaby@gmail.com>
2021-07-10 01:17:25 +0300

    fuc

--- lib/src/slider.dart
+++ lib/src/slider.dart
@@ -4,20 +4,20 @@ import 'package:carousel_slider/carousel_slider.dart';
 class MySlider extends StatelessWidget {
   Widget build(BuildContext context) {
     var items = "Fuck";
+    var width = MediaQuery.of(context).size.width;
     return CarouselSlider(
       options: CarouselOptions(
-        height: 350,
-        viewportFraction: 0.5,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        reverse: false,
-        autoPlay: false,
-        autoPlayInterval: Duration(seconds: 3),
-        autoPlayAnimationDuration: Duration(milliseconds: 800),
-        autoPlayCurve: Curves.fastOutSlowIn,
-        enlargeCenterPage: true,
-        scrollDirection: Axis.horizontal,
-      ),
+          height: MediaQuery.of(context).size.width * 0.45,
+          viewportFraction: 0.3,
+          initialPage: 0,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: false,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(milliseconds: 800),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          enlargeCenterPage: false,
+          scrollDirection: Axis.horizontal),
       items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) {
         return Builder(
           builder: (BuildContext context) {
@@ -27,7 +27,7 @@ class MySlider extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Colors.red, borderRadius: BorderRadius.circular(8)),
                 child: Text(
-                  items + ' $i',
+                  items + ' $width',
                   style: TextStyle(fontSize: 16.0),
                 ));
           },
