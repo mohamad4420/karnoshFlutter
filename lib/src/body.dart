a7bdf10aff5fa6f89c7a3290b05a12f7c1c3892c
basheershalabi123 <basheersalaby@gmail.com>
2021-07-11 00:36:23 +0300

    slider and search

--- lib/src/body.dart
+++ lib/src/body.dart
@@ -9,39 +9,21 @@ class Mybody extends StatelessWidget {
     return Container(
         child: ListView(
       children: <Widget>[
-        Text(
-          "Karnosh",
-          style: TextStyle(fontSize: 50),
-          textAlign: TextAlign.center,
-        ),
         Container(
-          decoration: BoxDecoration(
-              color: Colors.amber[100],
-              border: Border(bottom: BorderSide(width: 2, color: Colors.blue))),
           child: Text("action", style: TextStyle(fontSize: 35)),
         ),
-        MySlider(),
+        MySliderTest(),
         Container(
-          decoration: BoxDecoration(
-              color: Colors.amber[100],
-              border: Border(bottom: BorderSide(width: 2, color: Colors.blue))),
           child: Text("horror", style: TextStyle(fontSize: 35)),
         ),
-        MySlider(),
+        MySliderTest(),
         Container(
-          decoration: BoxDecoration(
-              color: Colors.amber[100],
-              border: Border(bottom: BorderSide(width: 2, color: Colors.blue))),
           child: Text("life", style: TextStyle(fontSize: 35)),
         ),
-        MySlider(),
+        MySliderTest(),
         Container(
-          decoration: BoxDecoration(
-              color: Colors.amber[100],
-              border: Border(bottom: BorderSide(width: 2, color: Colors.blue))),
           child: Text("children", style: TextStyle(fontSize: 35)),
         ),
-        MySlider(),
         MySliderTest()
       ],
     ));
