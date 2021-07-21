0c2d44f737006db845c29c26270178aff21cf920
basheershalabi123 <basheersalaby@gmail.com>
2021-07-21 20:29:12 +0300

    fix

--- lib/src/model/server_models.dart
+++ lib/src/model/server_models.dart
@@ -4,6 +4,11 @@ class SerModel {
   SerModel(this.server);
   SerModel.fromJson(Map<String, dynamic> map) {
     (map['server'] as List).map((data) {
+      String name = data;
+      if (name.contains(','))
+        this.nameServer.add(data.split(',')[1]);
+      else
+        this.nameServer.add(data.split('https://')[1].split('.')[0]);
       this.server.add(data);
     }).toList();
   }
