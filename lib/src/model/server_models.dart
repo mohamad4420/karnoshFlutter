366f32a9630a2ce4984b56acd07bdc0bcb27a525
mohamad abudaya <63648938+mohamad4420@users.noreply.github.com>
2021-07-21 20:36:48 +0300

    fix somting and change some colors

--- lib/src/model/server_models.dart
+++ lib/src/model/server_models.dart
@@ -5,16 +5,9 @@ class SerModel {
   SerModel.fromJson(Map<String, dynamic> map) {
     (map['server'] as List).map((data) {
       String name = data;
-<<<<<<< HEAD
       name = name.replaceAll("www.", "");
       name = name.split("//")[1].split(".")[0];
       this.nameServer.add(name);
-=======
-      if (name.contains(','))
-        this.nameServer.add(data.split(',')[1]);
-      else
-        this.nameServer.add(data.split('https://')[1].split('.')[0]);
->>>>>>> 0c2d44f737006db845c29c26270178aff21cf920
       this.server.add(data);
     }).toList();
   }
