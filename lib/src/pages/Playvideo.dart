a3ce7f5787ff8acb412bec86dbbc42080a5370b5
unknown <m1152580@gmail.com>
2021-08-15 21:58:17 +0300

    mohamad

--- lib/src/pages/Playvideo.dart
+++ lib/src/pages/Playvideo.dart
@@ -34,84 +34,96 @@ class _PlayVideoState extends State<PlayVideo> {
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   this.url = snapshot.data[0].server[0];
-                  return Scaffold(
-                      appBar: orientation == Orientation.portrait
-                          ? AppBar(
-                              leading: IconButton(
-                                onPressed: () {
-                                  SystemChrome.setPreferredOrientations([
-                                    DeviceOrientation.portraitUp,
-                                    DeviceOrientation.portraitDown,
-                                  ]);
-                                  Navigator.pop(context);
-                                },
-                                icon: Icon(Icons.arrow_back_ios),
-                              ),
-                              backgroundColor: Colors.black,
-                              title: IconButton(
-                                splashRadius: 25,
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => ServerList(
-                                              data: snapshot.data,
-                                              callback: (data) => {
-                                                    print(data),
-                                                    this.setState(() {
-                                                      this
-                                                          .webviewcontroller
-                                                          .loadUrl(data);
-                                                    })
-                                                  })));
-                                },
-                                icon: Icon(Icons.settings),
-                              ),
-                            )
-                          : null,
-                      body: Column(
-                        children: [
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: orientation == Orientation.portrait
-                                ? MediaQuery.of(context).size.width * 0.5625
-                                : MediaQuery.of(context).size.height,
-                            child: WebView(
-                                initialUrl: this.url,
-                                onWebViewCreated:
-                                    (WebViewController webViewController) {
-                                  webviewcontroller = webViewController;
-                                },
-                                javascriptMode: JavascriptMode.unrestricted,
-                                navigationDelegate:
-                                    (NavigationRequest request) {
-                                  return NavigationDecision.prevent;
-                                }),
-                          ),
-                          Container(
-                            child: orientation == Orientation.portrait
-                                ? Container(
-                                    width: MediaQuery.of(context).size.width,
-                                    height: MediaQuery.of(context).size.height -
-                                        MediaQuery.of(context).size.width *
-                                            0.5625 -
-                                        105.143,
-                                    child: ListView.builder(
-                                        itemCount: 10,
-                                        itemBuilder: (context, index) {
-                                          return Container(
-                                              child: Text('data$index'),
-                                              height: 100,
-                                              width: MediaQuery.of(context)
-                                                  .size
-                                                  .width,
-                                              color: Colors.amber);
-                                        }),
-                                  )
-                                : null,
-                          ),
-                        ],
-                      ));
+                  return WillPopScope(
+                    onWillPop: () async {
+                      SystemChrome.setPreferredOrientations([
+                        DeviceOrientation.portraitUp,
+                        DeviceOrientation.portraitDown,
+                      ]);
+                      Navigator.pop(context);
+                      return false;
+                    },
+                    child: Scaffold(
+                        appBar: orientation == Orientation.portrait
+                            ? AppBar(
+                                leading: IconButton(
+                                  onPressed: () {
+                                    SystemChrome.setPreferredOrientations([
+                                      DeviceOrientation.portraitUp,
+                                      DeviceOrientation.portraitDown,
+                                    ]);
+                                    Navigator.pop(context);
+                                  },
+                                  icon: Icon(Icons.arrow_back_ios),
+                                ),
+                                backgroundColor: Colors.black,
+                                title: IconButton(
+                                  splashRadius: 25,
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => ServerList(
+                                                data: snapshot.data,
+                                                callback: (data) => {
+                                                      print(data),
+                                                      this.setState(() {
+                                                        this
+                                                            .webviewcontroller
+                                                            .loadUrl(data);
+                                                      })
+                                                    })));
+                                  },
+                                  icon: Icon(Icons.settings),
+                                ),
+                              )
+                            : null,
+                        body: Column(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: orientation == Orientation.portrait
+                                  ? MediaQuery.of(context).size.width * 0.5625
+                                  : MediaQuery.of(context).size.height,
+                              child: WebView(
+                                  initialUrl: this.url,
+                                  onWebViewCreated:
+                                      (WebViewController webViewController) {
+                                    webviewcontroller = webViewController;
+                                  },
+                                  javascriptMode: JavascriptMode.unrestricted,
+                                  navigationDelegate:
+                                      (NavigationRequest request) {
+                                    return NavigationDecision.prevent;
+                                  }),
+                            ),
+                            Container(
+                              child: orientation == Orientation.portrait
+                                  ? Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      height: MediaQuery.of(context)
+                                              .size
+                                              .height -
+                                          MediaQuery.of(context).size.width *
+                                              0.5625 -
+                                          105.143,
+                                      child: ListView.builder(
+                                          itemCount: 10,
+                                          itemBuilder: (context, index) {
+                                            return Container(
+                                                child: Text('data$index'),
+                                                height: 100,
+                                                width: MediaQuery.of(context)
+                                                    .size
+                                                    .width,
+                                                color: Colors.amber);
+                                          }),
+                                    )
+                                  : null,
+                            ),
+                          ],
+                        )),
+                  );
                 } else {
                   return Scaffold(
                     appBar: AppBar(
